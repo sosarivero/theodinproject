d7d6781ae9edfd1ecbcae6392078f505b39a0db4
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-04-12 18:42:52 +0900

    Finish exercise 07

--- javascript-exercises/07_tempConversion/tempConversion.js
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const ftoc = function() {
+// I personally used .toFixed(1) because I found it
+// more readable than 'Math.round(formula * 10) * 10.
 
+const ftoc = function(fahrenheit) {
+  // [°C] = ([°F] − 32) × 5⁄9
+let conversion = ((fahrenheit - 32) * 5/9).toFixed(1);
+return Number(conversion);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  // [°F] = [°C] × 9⁄5 + 32	
+  conversion = (celsius * 9/5 + 32).toFixed(1);
+  return Number(conversion);
 };
 
+console.log(typeof (ftoc(32)))
 // Do not edit below this line
 module.exports = {
   ftoc,
