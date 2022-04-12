9c42f53f9f9b3b5d5ff5fb74ca011a8ecc9f1e82
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-04-12 18:21:59 +0900

    Finish exercise 05

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+  if (num1 < 0 || num2 < 0 || 
+    typeof num1 !== "number" || typeof num2 !== "number") {
+  return "ERROR";
+  }
+
+  let sum = 0;
+  let start;
+  let end;
+
+  // Dynamically decide start and end points for the loop
+  (num1 < num2) ? (start = num1, end = num2) :
+  (start = num2, end = num1);
+
+  for (let i = start; i <= end; i++) {
+  sum += i;
+}
+  return sum;
 };
 
 // Do not edit below this line
