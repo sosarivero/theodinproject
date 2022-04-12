85c2e6f96a3449b479d202cc8013233064fb4167
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-04-12 17:18:45 +0900

    Refactor exercise 03

--- javascript-exercises/03_reverseString/reverseString.js
+++ javascript-exercises/03_reverseString/reverseString.js
@@ -1,10 +1,5 @@
 const reverseString = function(string) {
-  arrayOfString = Array.from(string)
-  reversedString = "";
-  for (let i = arrayOfString.length - 1; i >= 0; i--) {
-    reversedString += arrayOfString[i];
-  }
-  return reversedString;
+  return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
