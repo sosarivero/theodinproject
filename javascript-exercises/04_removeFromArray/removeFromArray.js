2ebbced0a048d2dd4027b387ed5541bfc7b0d4e4
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-04-12 18:04:09 +0900

    Finish exercise 04

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...targets) {
+  // Make a clone of the array to avoid side effects.
+  const returnArray = [...array]
 
+  for (let target of targets) {
+    index = returnArray.indexOf(target);
+    if (index == -1) continue;
+
+    returnArray.splice(index, 1);
+  }
+
+  return returnArray;
 };
 
 // Do not edit below this line
