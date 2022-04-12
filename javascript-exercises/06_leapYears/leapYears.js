e2fa16c1b16bf1a8e8fa7716c27d30ccfc63467c
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-04-12 18:27:45 +0900

    Refactor exercise 06

--- javascript-exercises/06_leapYears/leapYears.js
+++ javascript-exercises/06_leapYears/leapYears.js
@@ -1,8 +1,5 @@
 const leapYears = function(year) {
-  if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-    return true;
-  }
-  return false;
+  return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
 };
 
 // Do not edit below this line
