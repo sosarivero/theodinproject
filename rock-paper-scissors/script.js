461554ae0a64b6b58b40e5daa76dd35b9625990a
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-04-14 01:06:50 +0900

    Show player and computer's play on DOM

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -4,10 +4,13 @@ function computerPlay() {
   let choice = Math.floor(Math.random() * 3);
   switch (choice) {
     case 0:
+      computerHand.textContent = "Rock";
       return "Rock";
     case 1:
+      computerHand.textContent = "Rock";
       return "Paper";
     case 2:
+      computerHand.textContent = "Rock";
       return "Scissors";
   }
 }
@@ -15,24 +18,31 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.substr(1).toLowerCase();
 
+  switch (playerSelection) {
+    case "Rock":
+      playerHand.textContent = "Rock";
+      break;
+    case "Paper":
+      playerHand.textContent = "Paper";
+      break;
+    case "Scissors":
+      playerHand.textContent = "Scissors";
+      break;
+  }
+
   if (playerSelection === computerSelection) {
     return "Draw!";
   } else if ((playerSelection === "Rock" && computerSelection === "Scissors")
           || (playerSelection === "Paper" && computerSelection === "Rock")
           || (playerSelection === "Scissors" && computerSelection === "Paper")) {
-            playerScoreCounter.textContent = playerScoreCounter.textContent.replace(scoreRegex, "★");
+            playerScoreCounter.textContent = playerScoreCounter.textContent.replace(/☆/, "★");
   } else {
-    computerScoreCounter.textContent = computerScoreCounter.textContent.replace(scoreRegex, "★");
+    computerScoreCounter.textContent = computerScoreCounter.textContent.replace(/☆/, "★");
   }
 }
 
 let playerScore = 0;
 let computerScore = 0;
-const ROUNDS = 5;
-
-const scoreRegex = /☆/;
-const scoreMark = "★"
-
 
 const playerScoreCounter = document.querySelector('#human-score');
 const computerScoreCounter = document.querySelector('#computer-score');
