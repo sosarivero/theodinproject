b3ab7a97d9dedbfdfb58540a1290b64a40de0370
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-04-14 11:25:54 +0900

    Add reset functionality

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -38,11 +38,34 @@ function playRound(playerSelection, computerSelection) {
           || (playerSelection === "Scissors" && computerSelection === "Paper")) {
             playerScoreCounter.textContent = playerScoreCounter.textContent.replace(/☆/, "★");
             gameState.textContent = `You win! ${playerSelection} beats ${computerSelection}.`
+            playerScore++;
   } else {
     computerScoreCounter.textContent = computerScoreCounter.textContent.replace(/☆/, "★");
     gameState.textContent = `You lose! ${playerSelection} loses to ${computerSelection}.`
+    computerScore++;
+  }
+
+  if (playerScore === 5 || computerScore === 5) {
+    let result = "";
+    (playerScore === 5) ? result = "WIN" : result = "LOSE";
+    gameState.textContent = `YOU ${result}! To play again, press the Reset button.`
+    Array.from(buttons).forEach(button => button.style.visibility = "hidden");
+    document.querySelector('.reset').style.visibility = "visible";
   }
 }
+
+function reset() {
+  playerScore = computerScore = 0;
+  playerScoreCounter.textContent = "☆☆☆☆☆"
+  computerScoreCounter.textContent = "☆☆☆☆☆"
+  gameState.textContent = "Game was reset!";
+
+  Array.from(buttons).forEach(button => button.style.visibility = "visible");
+  document.querySelector('.reset').style.visibility = "hidden";
+}
+
+let playerScore = 0;
+let computerScore = 0;
 
 const gameState = document.querySelector('#game-state')
 
@@ -51,3 +74,5 @@ const computerScoreCounter = document.querySelector('#computer-score');
 
 const playerHand = document.querySelector('#human-play');
 const computerHand = document.querySelector('#computer-play');
+
+const buttons = document.querySelectorAll(".choice");
