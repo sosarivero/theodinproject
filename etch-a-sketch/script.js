94516447d808f7f758bdad862cc1b31ae3f23704
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-04-14 17:24:26 +0900

    Use '**' in grid creation for simplifications

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -5,17 +5,10 @@ const telesketch = document.getElementById('telesketch');
 function createGrid(size) {
   telesketch.style.gridTemplateColumns = `repeat(${size}, 1fr)`
 
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < size ** 2; i++) {
     let gridCell = document.createElement('div');
     gridCell.className = 'grid-cell'
     telesketch.appendChild(gridCell);
-
-    for (let j = 1; j < size; j++) {
-    // Needs to repeat the createElement inside of this loop for it to work.
-    let gridCell = document.createElement('div');
-    gridCell.className = 'grid-cell'
-    telesketch.appendChild(gridCell);      
-    }
   }
 }
 
