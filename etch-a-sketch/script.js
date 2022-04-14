84ee2c2dbe9799ec974fd76bbc71a8e80fb206e2
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-04-14 17:20:27 +0900

    Create grid through JS

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,2 +1,22 @@
 "use strict";
 
+const telesketch = document.getElementById('telesketch');
+
+function createGrid(size) {
+  telesketch.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+
+  for (let i = 0; i < size; i++) {
+    let gridCell = document.createElement('div');
+    gridCell.className = 'grid-cell'
+    telesketch.appendChild(gridCell);
+
+    for (let j = 1; j < size; j++) {
+    // Needs to repeat the createElement inside of this loop for it to work.
+    let gridCell = document.createElement('div');
+    gridCell.className = 'grid-cell'
+    telesketch.appendChild(gridCell);      
+    }
+  }
+}
+
+createGrid(16);
