c6fb8b5b4d763ceac6243dce491cc9d3ccf248e0
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-04-14 17:45:49 +0900

    Add comment

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -8,8 +8,16 @@ function createGrid(size) {
   for (let i = 0; i < size ** 2; i++) {
     let gridCell = document.createElement('div');
     gridCell.className = 'grid-cell'
+
+    // Convert cells on black on hover.
+    gridCell.addEventListener('mouseenter', event => {
+      event.target.style.background = "black";
+    });
+
     telesketch.appendChild(gridCell);
   }
 }
 
 createGrid(16);
+
+
