7d30b5ff9f8fc63d20179b815f3e0774edfb5afe
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-04-19 21:57:26 +0900

    Finish exercise 09

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (s) {
+  let raw = '';
 
+  let pattern = /[A-Za-z]/;
+
+  [...s].forEach((char) => {
+    if (pattern.test(char)) {
+      raw += char;
+    }
+  });
+
+  raw = raw.toLowerCase();
+  reversed = raw.split('').reverse().join('');
+
+  for (let i = 0; i < raw.length; i++) {
+    if (reversed[i] !== raw[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
