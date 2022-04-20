d71bc353c13f86d2745140b840a2ea148a994fef
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-04-21 00:05:14 +0900

    Finish exercise 10

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) return 'OOPS';
 
+  let previous = 0;
+  let current = 1;
+
+  // i needs to start at 1 for the correct number of loops.
+  for (let i = 1; i < n; i++) {
+    // Create a temporary value in order to do all the switching of values.
+    let tmp = previous;
+    previous = current;
+    current += tmp;
+  }
+
+  return current;
 };
 
 // Do not edit below this line
