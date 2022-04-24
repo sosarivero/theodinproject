05f9f64644ab4d4552415aa7f742937b001c09c9
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-04-25 01:49:46 +0900

    Start grid design

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, operator, b) {
+  switch (operator) {
+    case '+':
+      add(a, b);
+      break;
+    case '-':
+      substract(a, b);
+      break;
+    case '*':
+      multiply(a, b);
+      break;
+    case '/':
+      divide(a, b);
+      break;
+  }
+}
