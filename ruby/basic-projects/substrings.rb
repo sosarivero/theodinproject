482580cd87f146cd36d4ddb51ff11bbc9a350867
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-05-06 13:51:49 +0900

    Add boilerplate for substrings project

--- ruby/basic-projects/substrings.rb
+++ ruby/basic-projects/substrings.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: false
 
 def substrings(string, dictionary) end
+
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+substrings('below', dictionary)
+# Expected output:
+# { "below" => 1, "low" => 1 }
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+# Expected output:
+# { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2,
+# "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
