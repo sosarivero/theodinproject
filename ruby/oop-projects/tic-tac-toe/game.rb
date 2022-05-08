71007f7bed2b346e4896740e1905b7bedf207e3a
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-05-09 07:40:44 +0900

    Start of player class

--- ruby/oop-projects/tic-tac-toe/game.rb
+++ ruby/oop-projects/tic-tac-toe/game.rb
@@ -1,26 +1,17 @@
 # frozen_string_literal: true
 
-# Class for tic-tac-toe game
+# Class for tic-tac-toe board-game
 class Board
-  attr_accessor :board
-  attr_reader :available_spaces
+  attr_accessor :board, :available_spaces
 
   def initialize
     # Create the following array: [nil, nil, nil, nil, nil, nil, nil, nil, nil]
     @board = Array.new(9)
-    @available_spaces = @board.map.with_index { |cell, i| i + 1 if cell.nil? }
+    @available_spaces = @board.map.with_index { |cell, i| i if cell.nil? }
   end
-
 
   def full?
     board.none?(nil)
-  end
-
-  def winner?(player_cells)
-    self.WIN_CONDITIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7] [2, 5, 8], [0, 4, 8], [3, 4, 6]]
-    WIN_CONDITIONS.each do |line|
-      return true if (player_cells - line).empty?
-    end
   end
 
   def to_s
@@ -30,11 +21,35 @@ class Board
     # 4 | 5 | 6
     # --+---+---
     # 7 | 8 | 9
-    p = @board.map.with_index { |cell, i| cell.nil? ? i + 1 : cell }
+    p = @board.map.with_index { |cell, i| cell.nil? ? i : cell }
     "#{p[0]} | #{p[1]} | #{p[2]}\n--+---+---\n#{p[3]} | #{p[4]} | #{p[5]}\n--+---+---\n#{p[6]} | #{p[7]} | #{p[8]}\n"
   end
 end
 
-hello = Board.new
-puts hello
-pp hello.available_spaces
+# Class for the tic-tac-toe players
+class Player
+  attr_reader :owned_cells
+
+  @@count = 0
+
+  def initialize(cpu: false)
+    @cpu = cpu
+    @mark = (@@count == 0) ? '◯' : '✗'
+    @@count += 1
+    @owned_cells = []
+  end
+
+  def move(board, cell_index)
+    if board.available_spaces.include?(cell_index)
+      board.board[cell_index] = @mark
+      @owned_cells.push(cell_index)
+    else
+      puts 'That cell is not available!'
+    end
+  end
+
+  def winner?
+    win_conditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [3, 4, 6]]
+    win_conditions.any? { |line| (line - @owned_cells).empty? }
+  end
+end
