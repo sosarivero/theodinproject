b90105b593bd5ba1dbc367b8a5b1f268cdb6cda5
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-05-09 05:33:13 +0900

    Add #winner? and #to_s to Board

--- ruby/oop-projects/tic-tac-toe/game.rb
+++ ruby/oop-projects/tic-tac-toe/game.rb
@@ -8,11 +8,26 @@ class Board
   def initialize
     # Create the following array: [nil, nil, nil, nil, nil, nil, nil, nil, nil]
     @board = Array.new(9)
+    @available_spaces = board.each_index { |i| i if board[i].nil? }
   end
-
-  @available_spaces = board.each_index { |i| i if board[i].nil? }
 
   def full?
     board.none?(nil)
   end
+
+  def winner?(player_cells)
+    self.WIN_CONDITIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7] [2, 5, 8], [0, 4, 8], [3, 4, 6]]
+    WIN_CONDITIONS.each do |line|
+      return true if (player_cells - line).empty?
+    end
+  end
+
+  def to_s
+    p = @board.map { |cell| cell.nil? ? ' ' : cell }
+
+    "#{p[0]} | #{p[1]} | #{p[2]}\n--+---+---\n#{p[3]} | #{p[4]} | #{p[5]}\n--+---+---\n#{p[6]} | #{p[7]} | #{p[8]}\n"
+  end
 end
+
+hello = Board.new
+puts hello
