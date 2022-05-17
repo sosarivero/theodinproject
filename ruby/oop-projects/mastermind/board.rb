1afc707a92cb2e74a61e67a3d329f02a3c75e875
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-05-17 20:12:35 +0900

    Added validation of guesses

--- ruby/oop-projects/mastermind/board.rb
+++ ruby/oop-projects/mastermind/board.rb
@@ -2,13 +2,22 @@
 
 # Class for the Mastermind decoding board
 class DecodingBoard
-  attr_reader :code
+  attr_reader :code, :holes, :repeats
+  attr_accessor :guesses
 
-  def initialize(code)
-    @code = code
+  def initialize(max_colors = 6, holes = 4, repeats: false)
+    @colors = ('A'...'Z').to_a[0...max_colors]
+    @holes = holes
+    @repeats = repeats
+    @guesses = []
   end
 
-  def check(guess)
+  def verify_guess(guess)
+    validate_length(guess) && validate_colors(guess) && @repeats || validate_uniqueness(guess)
+  end
+
+  # Compares a guess with the code to crack
+  def check_guess(guess)
     feedback = guess.split('').map.with_index do |char, i|
       if char == @code[i]
         'X'
@@ -18,7 +27,44 @@ class DecodingBoard
         '-'
       end
     end
-    # So the feedback always follows the same format and does not give information
-    feedback.sort.reverse.join(' ')
+    # Appends hash of guess => result to an array of guesses
+    @guesses << { guess => feedback.sort.reverse.join(' ') }
+  end
+
+  def make_code(computer: true)
+    @code = @colors.sample(4) if computer
+  end
+
+  private
+
+  def validate_colors(guess)
+    if (guess.split('') - @colors).empty?
+      true
+    else
+      puts "Incorrect input. List of valid colors: #{@colors}"
+      false
+    end
+  end
+
+  def validate_length(guess)
+    if guess.length == @holes
+      true
+    else
+      puts "Incorrect input. Guesses must be of length #{@holes}"
+      false
+    end
+  end
+
+  def validate_uniqueness(guess)
+    if guess.chars.uniq.count == @holes
+      true
+    else
+      puts 'Incorrect input. Colors cannot be repeated'
+      false
+    end
   end
 end
+
+board = DecodingBoard.new
+board.make_code
+pp board.verify_guess('AAAA')
