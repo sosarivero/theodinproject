d5c87793024d2a914f1f1931f230f1a67111cb05
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-06-01 18:26:17 +0900

    Do Event Manager tutorial up to iteration 3

---
+++ ruby/event_manager/lib/event_manager.rb
@@ -0,0 +1,18 @@
+require 'csv'
+puts 'EventManager initialized.'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+contents = CSV.open('event_attendees.csv',
+                    headers: true,
+                    header_converters: :symbol)
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
+end
