3882322ec33933d73cb295e18fd4d11684b3b9e8
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-06-05 15:32:13 +0900

    Do up to 'Outputting form letters to a file'

--- ruby/event_manager/lib/event_manager.rb
+++ ruby/event_manager/lib/event_manager.rb
@@ -1,18 +1,41 @@
 require 'csv'
-puts 'EventManager initialized.'
+require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
-contents = CSV.open('event_attendees.csv',
-                    headers: true,
-                    header_converters: :symbol)
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(address: zip, levels: 'country',
+                                              roles: %w[legislatorUpperBody legislatorLowerBody]).officials
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
 
 contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+  puts form_letter
 end
